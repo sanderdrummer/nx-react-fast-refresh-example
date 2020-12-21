a44b60babba34a6ecb19803ab6a82d2fce8f8f3d
sanderdrummer <tobias.pickel@moia.io>
2020-12-21 10:40:15 +0100

    feat: add fast refresh

--- apps/the-app/webpack.config.js
+++ apps/the-app/webpack.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const nrwlConfig = require("@nrwl/react/plugins/webpack.js");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const webpack = require("webpack");
 
 const { NX_ENV } = process.env;
@@ -8,11 +9,27 @@ const isDevelopment = NX_ENV === "development";
 module.exports = (config, _) => {
   nrwlConfig(config);
 
+  if (isDevelopment) {
+    //fast refresh
+    config.module.rules[0] = {
+      test: /\.([jt])sx?$/,
+      loader: "babel-loader",
+      exclude: /node_modules/,
+      options: {
+        babelrc: true,
+        cacheDirectory: true,
+        cacheCompression: false,
+        plugins: ["react-refresh/babel"],
+      },
+    };
+  }
+
   return {
     ...config,
     plugins: [
       ...config.plugins,
       isDevelopment && new webpack.HotModuleReplacementPlugin(),
+      isDevelopment && new ReactRefreshWebpackPlugin(),
     ].filter(Boolean),
     devServer: {
       ...config.devServer,
