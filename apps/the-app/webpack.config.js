3e80d0ca2bc6a6f8af0e6fc164a9417617abcd7c
sanderdrummer <tobias.pickel@moia.io>
2020-12-21 10:34:55 +0100

    feat: add development env variables

--- apps/the-app/webpack.config.js
+++ apps/the-app/webpack.config.js
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const nrwlConfig = require("@nrwl/react/plugins/webpack.js");
 
+const { NX_ENV } = process.env;
+const isDevelopment = NX_ENV === "development";
+
 module.exports = (config, _) => {
   nrwlConfig(config);
+
   return config;
 };
