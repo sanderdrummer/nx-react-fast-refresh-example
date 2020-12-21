7a6145cf55c015340ce8f74fbe6c0dc36ea17d09
sanderdrummer <tobias.pickel@moia.io>
2020-12-21 10:37:49 +0100

    feat: add hmr

--- apps/the-app/webpack.config.js
+++ apps/the-app/webpack.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const nrwlConfig = require("@nrwl/react/plugins/webpack.js");
+const webpack = require("webpack");
 
 const { NX_ENV } = process.env;
 const isDevelopment = NX_ENV === "development";
@@ -7,5 +8,15 @@ const isDevelopment = NX_ENV === "development";
 module.exports = (config, _) => {
   nrwlConfig(config);
 
-  return config;
+  return {
+    ...config,
+    plugins: [
+      ...config.plugins,
+      isDevelopment && new webpack.HotModuleReplacementPlugin(),
+    ].filter(Boolean),
+    devServer: {
+      ...config.devServer,
+      hot: true,
+    },
+  };
 };
